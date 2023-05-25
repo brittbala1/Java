e1cf76bd3aa1ef8a6e3c25c8aa44243e189b4383
brittbala1 <balacloud30@gmail.com>
2023-05-25 20:38:29 +0530

    commited by Bala

---
+++ Student.java
@@ -0,0 +1,19 @@
+class Student
+{
+
+public static void main (String[]args)
+
+{
+
+School school = new School();
+school.teach();
+
+System.out.println(teacher.salary);
+school.recruit();
+
+}
+
+
+
+
+}
